c3fe6d6ef5cbffb1c3c3ae167c68aa49a37ee0a1
tadeulps <tadeslopes@gmail.com>
2021-04-03 03:14:50 -0300

    botão verde ao selecionar as 3 opções adicionado

--- script.js
+++ script.js
@@ -2,15 +2,21 @@
   //const elemento=document.querySelector(".opcoes");
   //elemento.classList.toggle('selecionado')
 //}
-
+let teste1=parseInt(0);
+let teste2=parseInt(0);
+let teste3=parseInt(0);
 function selecionarComida(classeBotao){
-  const selecionado = document.querySelector(".comida .selecionado");
-  if(selecionado!==null){
-    selecionado.classList.remove('selecionado')
+  const comida = document.querySelector(".comida .selecionado");
+  if(comida!==null){
+    
+    comida.classList.remove('selecionado')
   }
   const seletor = "." + classeBotao;  
   const botao=document.querySelector(seletor);
   botao.classList.toggle('selecionado');
+  teste1=1;
+  final()
+  
 }
 //const seletor = "." + classeBotao;
 //const botao = document.querySelector(seletor);
@@ -23,6 +29,8 @@ function selecionarBebida(classeBotao){
   const seletor = "." + classeBotao;  
   const botao=document.querySelector(seletor);
   botao.classList.toggle('selecionado');
+  teste2=2;
+  final()
 }
 function selecionarSobremesa(classeBotao){
   const selecionado = document.querySelector(".sobremesa .selecionado");
@@ -32,4 +40,18 @@ function selecionarSobremesa(classeBotao){
   const seletor = "." + classeBotao;  
   const botao=document.querySelector(seletor);
   botao.classList.toggle('selecionado');
+  teste3=3;
+  final()
+  
 }
+
+function final(){
+  if(teste1==1 && teste2==2 && teste3==3){
+    alert(teste1);
+    const inf =document.querySelector(".botao");
+    inf.innerHTML="Fechar pedido"
+    inf.classList.add('clicavel');
+
+}
+}
+
